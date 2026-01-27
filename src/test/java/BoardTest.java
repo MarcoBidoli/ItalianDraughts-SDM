7070a16b111881f45fa72ea04eede74e94bb485c
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-01-27 16:57:17 +0100

    feat: set and reset for game (putting on pieces)

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -21,4 +21,37 @@ public class BoardTest {
         Board board = new Board();
         assertEquals(true, board.isEmpty());
     }
+
+    @Test
+    public void countPieces() {
+        Board board = new Board();
+        board.setGame();
+        int c = 0;
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if(!board.getCell(i,j).isEmpty())
+                    c++;
+            }
+        }
+        assertEquals(24, c);
+    }
+
+    @Test
+    public void countPiecesColor() {
+        Board board = new Board();
+        board.setGame();
+        int w = 0, b = 0;
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if(!board.getCell(i,j).isEmpty()) {
+                    if(board.getCell(i,j).getPiece().getColor() == Color.WHITE)
+                        w++;
+                    if(board.getCell(i,j).getPiece().getColor() == Color.BLACK)
+                        b++;
+                }
+            }
+        }
+        assertEquals(12, w);
+        assertEquals(12, b);
+    }
 }
