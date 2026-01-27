81c4fbde34030e275fecf8ce63862822b488741d
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-01-27 15:30:19 +0100

    feat: first Board implementation

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,7 +1,8 @@
 public class Board {
-    Cell[][] board = new Cell[8][8];
+    Cell[][] board;
 
     public Board() {
+        board = new Cell[8][8];
         initCells();
     }
 
@@ -9,9 +10,9 @@ public class Board {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if ((i + j) % 2 == 0) {
-                    board[i][j].setColor(Color.BLACK);
+                    board[i][j] = new Cell(Color.BLACK);
                 } else {
-                    board[i][j].setColor(Color.WHITE);
+                    board[i][j] = new Cell(Color.WHITE);
                 }
             }
         }
@@ -20,4 +21,30 @@ public class Board {
     public Color getCellColor(int x, int y) {
         return board[x][y].getColor();
     }
+
+    public Cell getCell(int x, int y) {
+        return board[x][y];
+    }
+
+    private void emptyBoard() {
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if(!board[i][j].isEmpty()) {
+                    board[i][j].empty();
+                }
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if(!board[i][j].isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
