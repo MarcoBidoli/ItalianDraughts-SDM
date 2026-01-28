da2850aa4412a13f599fe1cd6ab211fc53df2349
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-01-28 16:53:29 +0100

    feat: eating logic

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -23,38 +23,63 @@ public class Action {
     }
 
     private List<List<Move>> eating () {
-        List<List<Move>> eatings = new ArrayList<>();
+        List<List<Move>> allEatings = new ArrayList<>();
         for(int i=0; i<8; i++) {
-            for (int j = 2; j < 6; j++) { //pieces in columns 2-5 have the "standard" eating
+            for (int j = 0; j < 8; j++) {
                 List<Move> eatingsXY = new ArrayList<>();
                 if (gameBoard.getCell(i, j).getPiece().getColor().equals(player)) {
-                    if(){
-                        break;
-                    }else {
-                        eatingsXY.add(standardEatingL(i, j));
-                        eatingsXY.add(standardEatingR(i, j));
-                    }
+                    findEatings(i, j, new ArrayList<>(), allEatings);
                 }
-                eatings.add(eatingsXY);
             }
         }
+        return allEatings;
     }
 
-    private Move standardEatingL(int i, int j) {
-        if (!gameBoard.getCell((i+1)*direction, j-1).isEmpty()) {
-            if(gameBoard.getCell((i+2)*direction, j-2).isEmpty()) {
-                return new Move(i, j, (i+2)*direction, j-2);
+    private void findEatings(int x, int y, List<Move> eatings, List<List<Move>> allEatings) {
+        boolean canEat = false;
+
+        int[] dirs = new int[]{1, -1};
+        for(int i : dirs) {
+            int xOpp = x + direction;
+            int yOpp = y + i;
+            int finX = x + 2*direction;
+            int finY = y + 2*i;
+            if(canEat(x, y, xOpp, yOpp, finX, finY)){
+                canEat = true;
+                eatings.add(new Move(x, y, finX, finY));
+                Piece eaten = gameBoard.getCell(xOpp, yOpp).getPiece();
+                gameBoard.getCell(xOpp, yOpp).empty();
+                Piece moved = gameBoard.getCell(x, y).getPiece();
+                gameBoard.getCell(x, y).empty();
+                gameBoard.getCell(finX, finY).putPieceOn(moved);
+                findEatings(finX, finY, eatings, allEatings);
+
+                gameBoard.getCell(finX, finY).empty();
+                gameBoard.getCell(x, y).putPieceOn(moved);
+                eatings.remove(eatings.size() - 1);
+                gameBoard.getCell(xOpp, yOpp).putPieceOn(eaten);
             }
         }
-        return null;
+
+        if(!canEat && !eatings.isEmpty())
+            allEatings.add(new ArrayList<>(eatings));
     }
-    private Move standardEatingR(int i, int j) {
-        if (!gameBoard.getCell((i+1)*direction, j+1).isEmpty()) {
-            if(gameBoard.getCell((i+2)*direction, j+-2).isEmpty()) {
-                return new Move(i, j, (i+2)*direction, j+2);
-            }
-        }
-        return null;
+
+    private boolean canEat(int x, int y, int xOpp, int yOpp, int finX, int finY) {
+        //TODO: substitute with isOnBoard Board method
+        if(finX < 0 || finX >= 8 || finY < 0 || finY >= 8)
+            return false;
+
+        if(gameBoard.getCell(xOpp, yOpp).isEmpty() && gameBoard.getCell(xOpp, yOpp).getPiece().getColor() == player)
+            return false;
+
+        if(!gameBoard.getCell(finX, finY).isEmpty())
+            return false;
+
+        if(!gameBoard.getCell(x,y).getPiece().isKing() && gameBoard.getCell(xOpp,yOpp).getPiece().isKing())
+            return false;
+
+        return true;
     }
 
     private List<List<Move>> moving() {
