f669825ae0dedffffca2c16e4964fe3a19700fad
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-01-28 18:24:07 +0100

    func: eating test

---
+++ src/test/java/EatingLegalMovesTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class EatingLegalMovesTest {
+    @Test
+    public void singleEat() throws InvalidMoveException {
+        Board board = new Board();
+        board.placePiece(Color.WHITE, 5, 2);
+        board.placePiece(Color.BLACK, 4, 3);
+    }
+}
