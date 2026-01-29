27cedd886afd12ec757ab0042fce1e1cabacff8a
Fede Castelli <feder0210@gmail.com>
2026-01-29 12:29:18 +0100

    Test: removed useless lines

--- src/test/java/ActualMovingTest.java
+++ src/test/java/ActualMovingTest.java
@@ -11,7 +11,6 @@ public class ActualMovingTest {
     public void testMoving() throws InvalidMoveException {
         board.initCells();
         board.placePiece(Color.WHITE, 4, 4);
-        Action action = new Action(board, Color.WHITE);
         List<Move> moves = new ArrayList<>();
         moves.add(new Move(4, 4, 3, 3));
         Game game = new Game();
@@ -24,7 +23,6 @@ public class ActualMovingTest {
         board.initCells();
         board.placePiece(Color.WHITE, 4, 4);
         board.placePiece(Color.BLACK, 3, 3);
-        Action action = new Action(board, Color.WHITE);
         List<Move> moves = new ArrayList<>();
         moves.add(new Move(4, 4, 2, 2));
         Game game = new Game();
@@ -39,7 +37,6 @@ public class ActualMovingTest {
         board.placePiece(Color.WHITE, 6, 6);
         board.placePiece(Color.BLACK, 5, 5);
         board.placePiece(Color.BLACK, 3, 3);
-        Action action = new Action(board, Color.WHITE);
         List<Move> moves = new ArrayList<>();
         moves.add(new Move(6, 6, 4, 4));
         moves.add(new Move(4, 4, 2, 2));
@@ -58,7 +55,6 @@ public class ActualMovingTest {
         board.placePiece(Color.BLACK, 5, 5);
         board.placePiece(Color.BLACK, 3, 3);
         board.placePiece(Color.BLACK, 1, 1);
-        Action action = new Action(board, Color.WHITE);
         List<Move> moves = new ArrayList<>();
         moves.add(new Move(6, 6, 4, 4));
         moves.add(new Move(4, 4, 2, 2));
