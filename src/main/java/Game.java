56c975fd01bbf60082b3be128d5221ecbbfa56fe
Fede Castelli <feder0210@gmail.com>
2026-01-29 12:31:13 +0100

    feat: added movePiece method to have the ability to move pieces on a board (given the move)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,19 @@
+import java.util.List;
+
 public class Game {
     private Board gameBoard;
+
+    public Board movePieces(List<Move> move, Board board) throws InvalidMoveException {
+        while(!move.isEmpty()) {
+            Move currentMove = move.removeFirst();
+            Piece pieceToMove = board.getCell(currentMove.fromRow, currentMove.fromCol).getPiece();
+            board.getCell(currentMove.toRow, currentMove.toCol).putPieceOn(pieceToMove);
+            board.getCell(currentMove.fromRow, currentMove.fromCol).empty();
+            if(Math.abs(currentMove.fromRow - currentMove.toRow) == 2){
+                board.getCell((currentMove.fromRow + currentMove.toRow)/2, (currentMove.toCol + currentMove.fromCol)/2).empty();
+                movePieces(move, board);
+            }
+        }
+        return board;
+    }
 }
