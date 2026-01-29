4f30d66819ac7e4e9d74f8b8cd9dcb101cf83a5f
Fede Castelli <feder0210@gmail.com>
2026-01-29 12:27:58 +0100

    Test: added another test for more possible moves on a board

--- src/test/java/ActualMovingTest.java
+++ src/test/java/ActualMovingTest.java
@@ -34,7 +34,7 @@ public class ActualMovingTest {
         assertSame(Color.WHITE, board.getCell(2, 2).getPiece().getColor());
     }
     @Test
-    public void testMultipleEating() throws InvalidMoveException {
+    public void testDoubleEating() throws InvalidMoveException {
         board.initCells();
         board.placePiece(Color.WHITE, 6, 6);
         board.placePiece(Color.BLACK, 5, 5);
@@ -50,5 +50,27 @@ public class ActualMovingTest {
         assertTrue(board.getCell(4,4).isEmpty());
         assertTrue(board.getCell(3,3).isEmpty());
         assertSame(Color.WHITE, board.getCell(2, 2).getPiece().getColor());
+    }
+    @Test
+    public void testTripleEating() throws InvalidMoveException {
+        board.initCells();
+        board.placePiece(Color.WHITE, 6, 6);
+        board.placePiece(Color.BLACK, 5, 5);
+        board.placePiece(Color.BLACK, 3, 3);
+        board.placePiece(Color.BLACK, 1, 1);
+        Action action = new Action(board, Color.WHITE);
+        List<Move> moves = new ArrayList<>();
+        moves.add(new Move(6, 6, 4, 4));
+        moves.add(new Move(4, 4, 2, 2));
+        moves.add(new Move(2, 2, 0, 0));
+        Game game = new Game();
+        game.movePieces(moves, board);
+        assertTrue(board.getCell(6,6).isEmpty());
+        assertTrue(board.getCell(5,5).isEmpty());
+        assertTrue(board.getCell(4,4).isEmpty());
+        assertTrue(board.getCell(3,3).isEmpty());
+        assertTrue(board.getCell(2,2).isEmpty());
+        assertTrue(board.getCell(1,1).isEmpty());
+        assertSame(Color.WHITE, board.getCell(0, 0).getPiece().getColor());
     }
 }
