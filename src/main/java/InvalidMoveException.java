3d2546196ca7067eb5ec504d3a8466b7bcf84c1a
Marco Bidoli <marco.bidoli@gmail.com>
2026-01-29 12:04:18 +0100

    docs: implemented Javadoc for InvalidMoveException

--- src/main/java/InvalidMoveException.java
+++ src/main/java/InvalidMoveException.java
@@ -1,4 +1,12 @@
+/**
+ * Thrown to indicate that a requested move or piece placement violates
+ * the board's spatial constraints.
+ **/
 public class InvalidMoveException extends Exception {
+    /**
+     * Constructs a new exception with a specific detail message.
+     * @param msg The description of why the move was considered invalid.
+     */
     public InvalidMoveException(String msg) {
         super(msg);
     }
