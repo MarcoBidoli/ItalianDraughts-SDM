b00d358d57050e84b9043c832e3bc47c64749c6f
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-01-29 20:36:11 +0100

    func: completd eatings (now eatings are computed and sorted by priorities given by rules); implemented eatingMove to model whether a king is eaten

---
+++ src/main/java/EatingMove.java
@@ -0,0 +1,12 @@
+public class EatingMove extends Move{
+    private boolean kingEaten;
+
+    public EatingMove(int fromRow, int fromCol, int toRow, int toCol, boolean kingEaten) throws IllegalArgumentException {
+        super(fromRow, fromCol, toRow, toCol);
+        this.kingEaten = kingEaten;
+    }
+
+    public boolean kingEaten(){
+        return kingEaten;
+    }
+}
