8aa90aa8f6a0dc9ee8a8610a7f9e59aca47a41a9
Alessandro De Nardo <alessandrodenardo@hotmail.com>
2026-02-03 00:02:57 +0100

    Added draw test

---
+++ src/test/java/GameDrawTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameDrawTest {
+
+    @Test
+    void drawTriggeredAfter40QuietMovesEachWhenBothHaveKings() throws InvalidMoveException {
+        Board board = new Board();
+        board.initCells();
+
+        // Mettiamo un solo pezzo bianco e uno nero e li rendiamo king
+        board.placePiece(Color.WHITE, 4, 4);
+        board.getCell(4, 4).getPiece().setKing(true);
+
+        board.placePiece(Color.BLACK, 3, 3);
+        board.getCell(3, 3).getPiece().setKing(true);
+
+        Game game = new Game();
+
+        Board gb = game.getBoard();
+        gb.initCells();
+        gb.placePiece(Color.WHITE, 4, 4);
+        gb.getCell(4, 4).getPiece().setKing(true);
+        gb.placePiece(Color.BLACK, 3, 3);
+        gb.getCell(3, 3).getPiece().setKing(true);
+
+        // Ora muoviamo i due king avanti/indietro senza catture.
+        // White: (4,4) <-> (5,5)
+        // Black: (3,3) <-> (2,2)
+        for (int i = 0; i < 40; i++) {
+            // turno White
+            List<Move> w = new ArrayList<>();
+            if (i % 2 == 0) w.add(new Move(4, 4, 5, 5));
+            else w.add(new Move(5, 5, 4, 4));
+            game.applyTurn(w);
+
+            // turno Black
+            List<Move> b = new ArrayList<>();
+            if (i % 2 == 0) b.add(new Move(3, 3, 2, 2));
+            else b.add(new Move(2, 2, 3, 3));
+            game.applyTurn(b);
+        }
+
+        assertEquals(GameStatus.DRAW, game.getStatus());
+    }
+}
