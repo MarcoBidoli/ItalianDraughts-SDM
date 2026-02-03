c72a21a5f39dc7947ad382237b1c15ba9b7f7b2c
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-02-03 16:11:56 +0100

    refactor: renamed enum Color to avoid homonymy with java awt class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,6 @@ public class Main {
 
     void main() throws InvalidMoveException {
 
-        Board b = new Board();
-        b.setGame();
-
-        JFrame frame = new JFrame("Dama Java");
-        BoardPanel bP = new BoardPanel(b);
-
-        frame.add(bP);
-        frame.setSize(655, 680);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
         List<List<Move>> currentPlayerLegalMoves = new ArrayList<>();
 
 
