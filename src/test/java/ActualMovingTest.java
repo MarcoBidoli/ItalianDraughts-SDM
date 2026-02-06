0378e99c48d6f0d00335a636483ad82cdad3f8fb
Marco Bidoli <marco.bidoli@gmail.com>
2026-02-06 16:32:40 +0100

    refactor: fixed declaration-redundancy code smells

--- src/test/java/ActualMovingTest.java
+++ src/test/java/ActualMovingTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ActualMovingTest {
-    Board board = new Board();
+    private final Board board = new Board();
     @Test
     public void testMoving() throws InvalidMoveException {
         board.initCells();
