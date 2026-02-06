145225902b829a9eb1b450afdd5ef23353196011
Marco Bidoli <marco.bidoli@gmail.com>
2026-02-06 15:53:43 +0100

    refactor: removed control flow and data flow issued related to isOnBoard

--- src/main/java/italian_draughts/domain/Board.java
+++ src/main/java/italian_draughts/domain/Board.java
@@ -97,7 +97,11 @@ public class Board {
     }
 
     // Added for action branch support
-    public boolean isOnBoard(int i, int j) {
+    public boolean isNotOnBoard(int i, int j) {
+        return !onBoard(i, j);
+    }
+
+    private static boolean onBoard(int i, int j) {
         return i >= 0 && i < 8 && j >= 0 && j < 8;
     }
 
