96c06552b1373ec634ee52a918bc3f977fc232b4
Fede Castelli <feder0210@gmail.com>
2026-02-07 17:10:54 +0100

    fix: rewritten better tests

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -43,10 +43,19 @@ public class BoardTest {
     }
 
     @Test
-    public void printBoard() {
+    public void printBoard() { //Questo metodo non testa niente, scegliere se tenere
         Board board = new Board();
         board.setGame();
         board.printBoard();
+        String correctString = "0  1  2  3  4  5  6  7\n" +
+                "0  ⛂  .  ⛂  .  ⛂  .  ⛂  . \n" +
+                "1  .  ⛂  .  ⛂  .  ⛂  .  ⛂ \n" +
+                "2  ⛂  .  ⛂  .  ⛂  .  ⛂  . \n" +
+                "3  .  .  .  .  .  .  .  . \n" +
+                "4  .  .  .  .  .  .  .  . \n" +
+                "5  .  ⛀  .  ⛀  .  ⛀  .  ⛀ \n" +
+                "6  ⛀  .  ⛀  .  ⛀  .  ⛀  . \n" +
+                "7  .  ⛀  .  ⛀  .  ⛀  .  ⛀";
     }
     @Test
     public void getBoardRepresentationTest() throws InvalidMoveException {
