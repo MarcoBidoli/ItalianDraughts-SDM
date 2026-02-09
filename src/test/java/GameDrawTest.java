d1203d5f342471cf6d5a8395cd955817e9fb48ee
Alessandro De Nardo <alessandrodenardo@hotmail.com>
2026-02-09 16:14:40 +0100

    test: refactor test drawTriggeredAfter40QuietMovesWhenBothHaveKings()

--- src/test/java/GameDrawTest.java
+++ src/test/java/GameDrawTest.java
@@ -2,7 +2,6 @@ import italian_draughts.domain.*;
 import italian_draughts.logic.Game;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
