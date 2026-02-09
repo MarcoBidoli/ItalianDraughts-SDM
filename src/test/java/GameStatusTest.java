ab0fb4e916a6b105767aa12b142fb5924cf8a387
Marco Bidoli <marco.bidoli@gmail.com>
2026-02-09 09:39:24 +0100

    feat: test for win check when no moves are available

--- src/test/java/GameStatusTest.java
+++ src/test/java/GameStatusTest.java
@@ -4,6 +4,8 @@ import italian_draughts.domain.GameStatus;
 import italian_draughts.domain.Move;
 import italian_draughts.domain.InvalidMoveException;
 import italian_draughts.logic.Game;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -39,5 +41,27 @@ public class GameStatusTest {
         game.processTurn(List.of(new Move(5, 1, 3, 3)));
 
         assertEquals(GameStatus.WHITE_WINS, game.getStatus());
+    }
+
+    @Test
+    public void testWinConditionWhenNoMovesAvailable() throws InvalidMoveException {
+        Game game = new Game();
+        Board board = game.getBoard();
+        // board where Black is one move away from being trapped
+        // and White is about to make that move.
+        board.placeKing(GameColor.WHITE, 1, 7);
+        board.placeKing(GameColor.WHITE, 2, 6);
+        board.placePiece(GameColor.BLACK, 0, 6);
+        board.printBoard();
+        game.setCurrentTurn(GameColor.WHITE);
+        List<Move> whiteWinningMove = new ArrayList<>(List.of(new Move(2, 6,1, 5)));
+
+        // Process the turn
+        game.processTurn(whiteWinningMove);
+        board.printBoard();
+
+        // The turn should have switched to Black internally,
+        // and checkWin() should have seen Black has no moves.
+        assertEquals(GameStatus.WHITE_WINS, game.getStatus(),"White should win if Black has no legal moves left.");
     }
 }
