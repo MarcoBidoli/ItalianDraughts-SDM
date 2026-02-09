c43979101500ee92bfd0432aa2c3ccd550610ef2
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-02-09 12:48:19 +0100

    feat: class PaletteColors to model Board Colors

---
+++ src/main/java/italian_draughts/gui/PaletteColors.java
@@ -0,0 +1,22 @@
+package italian_draughts.gui;
+
+import java.awt.*;
+
+public class PaletteColors {
+    protected final Color WOOD_LIGHT = new Color(223, 191, 159);
+    protected final Color WOOD_DARK = new Color(153, 102, 51);
+    protected final Color WOOD_MARGIN = new Color(115, 74, 33);
+    protected final Color PIECE_WHITE = new Color(245, 245, 245);
+    protected final Color PIECE_BLACK = new Color(35, 35, 35);
+    protected final Color HIGHLIGHT_MOVE = new Color(46, 204, 113, 180);
+    protected final Color SELECTED_TILE = new Color(255, 255, 0, 120);
+    protected final Color GOLD = new Color(255, 215, 0);
+
+    public Color getWOOD_MARGIN() {
+        return WOOD_MARGIN;
+    }
+
+    public Color getWOOD_DARK() {
+        return WOOD_DARK;
+    }
+}
