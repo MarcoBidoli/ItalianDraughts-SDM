41100a4bdce7980666c841ab2832234b1777258f
ElisabettaGrandi <elisabettagrandi03@gmail.com>
2026-02-10 17:15:15 +0100

    feat: implemented controller

--- src/main/java/italian_draughts/logic/Game.java
+++ src/main/java/italian_draughts/logic/Game.java
@@ -66,6 +66,7 @@ public class Game {
 
         // 4) Check win (pezzi finiti o nessuna mossa legale per il nuovo currentPlayer)
         checkWin();
+        notifyObservers();
     }
 
     private boolean hasKing(GameColor color) {
@@ -237,10 +238,6 @@ public class Game {
 
     public void addObserver(GameObserver gameObserver) {
         observers.add(gameObserver);
-    }
-
-    public void removeObserver(GameObserver gameObserver) {
-        observers.remove(gameObserver);
     }
 
     private void notifyObservers(){
